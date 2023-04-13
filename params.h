b42ede7e54c24968252240c1cb45f66142261dcb
PionTsai <piontsai@gmail.com>
2023-04-13 11:38:35 +0800

    Create "poly.h"

--- params.h
+++ params.h
@@ -2,6 +2,7 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
+#define N 256
 #define Q 8380417
 #define D 13
 
---
+++ poly.h
@@ -0,0 +1,11 @@
+#ifndef POLY_H
+#define POLY_H
+
+#include <stdint.h>
+#include "params.h"
+
+typedef struct {
+  int32_t coeffs[N];
+} poly;
+
+#endif
